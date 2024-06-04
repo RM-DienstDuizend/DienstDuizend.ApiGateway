1b8f8a4edd27e5d5d4dd07fde43d20a119b9f539
Robin Mager <62018777+BlackBearFTW@users.noreply.github.com>
2024-06-04 13:00:19 +0200

    Moved from devops to github

---
+++ DienstDuizend.ApiGateway/Program.cs
@@ -0,0 +1,100 @@
+using System.Net;
+using System.Text;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using OpenTelemetry.Metrics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath +
+                 "/Settings") // the path where the json file should be loaded from
+    .AddJsonFile("gateway.json", optional: true);
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder => builder
+        // .AddConsoleExporter()
+        .AddPrometheusExporter() // We use v1.7 because currently v1.8 has an issue with formatting.
+        .AddAspNetCoreInstrumentation()
+);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JwtAuthSettings:AccessTokenSecret"))),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    // This will make it so authentication is always required, unless specified otherwise.
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+builder.Services.AddCors(p => p.AddPolicy("corsSettings",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "localhost:5173").AllowAnyMethod()
+            .AllowAnyHeader().AllowCredentials();
+    }));
+
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    
+    options.AddPolicy("fixed-by-user", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.User.Identity?.Name?.ToString(),
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 10,
+                Window = TimeSpan.FromSeconds(20)
+            }));
+    
+    // The rate limiting per ip address is higher because this is more vunerable
+    // to a ddos attack by anonymous users
+    options.AddPolicy("fixed-by-ip", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString(),
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 10,
+                Window = TimeSpan.FromSeconds(60)
+            }));
+});
+
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.UseCors("corsSettings");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", async () => "Hello World");
+
+app.MapPrometheusScrapingEndpoint().AllowAnonymous();
+
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+app.Run();
